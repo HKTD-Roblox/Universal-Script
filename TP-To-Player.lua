403b1e29f449f0bb5606fb3b13ce495cf07a5c20
Hoàng Kim Tiến Đạt <thuykieu071980@gmail.com>
2026-07-19 13:35:03 +0700

    Update TP-To-Player.lua

--- TP-To-Player.lua
+++ TP-To-Player.lua
@@ -3,7 +3,7 @@
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "TELEPORT TO PLAYER",
     Text = "By HKTD",
-	  Icon = "rbxassetid://132292718620518",
+	  Icon = "rbxassetid://123653870026944",
     Duration = 5
 })
 
