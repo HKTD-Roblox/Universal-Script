6ad6586a0a6ca3b05708e409aac1153d7c32d196
Hoàng Kim Tiến Đạt <thuykieu071980@gmail.com>
2026-07-19 14:27:06 +0700

    Create Auto-Clicker.lua

---
+++ Auto-Clicker.lua
@@ -0,0 +1,10 @@
+--Script By HKTD, TikTok: https://www.tiktok.com/@hktd_roblox
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Auto Clicker",
+    Text = "By HKTD",
+	Icon = "rbxassetid://123653870026944",
+    Duration = 5
+})
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostPlayer352/Test4/refs/heads/main/Auto%20Clicker"))()
