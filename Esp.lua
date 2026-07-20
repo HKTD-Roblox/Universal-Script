c41b4615f3fa5bbe9c4bdb2f6037c2e09ef51aeb
Hoàng Kim Tiến Đạt <thuykieu071980@gmail.com>
2026-07-20 12:28:36 +0700

    Update Esp.lua

--- Esp.lua
+++ Esp.lua
@@ -1,6 +1,6 @@
 --Script By HKTD, TikTok: https://www.tiktok.com/@hktd_roblox
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HKTD-Roblox/Script-Beta/refs/heads/main/Mobile-Keyborad.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HKTD-Roblox/Script-Beta/refs/heads/main/Script-By-HKTD.lua", true))()
 
 -- [ESP SCRIPT]
 local Players = game:GetService("Players")
