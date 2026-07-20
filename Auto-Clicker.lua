b5c4971898726207ff23d7aea16295a00f69afa9
Hoàng Kim Tiến Đạt <thuykieu071980@gmail.com>
2026-07-20 12:28:27 +0700

    Update Auto-Clicker.lua

--- Auto-Clicker.lua
+++ Auto-Clicker.lua
@@ -1,5 +1,5 @@
 --Script By HKTD, TikTok: https://www.tiktok.com/@hktd_roblox
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HKTD-Roblox/Script-Beta/refs/heads/main/Mobile-Keyborad.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HKTD-Roblox/Script-Beta/refs/heads/main/Script-By-HKTD.lua", true))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostPlayer352/Test4/refs/heads/main/Auto%20Clicker"))()
