8655f90f595efb4ed37611dfbd63d3e2c87f86c7
Hoàng Kim Tiến Đạt <thuykieu071980@gmail.com>
2026-07-20 12:26:06 +0700

    Update Auto-Clicker.lua

--- Auto-Clicker.lua
+++ Auto-Clicker.lua
@@ -1,10 +1,5 @@
 --Script By HKTD, TikTok: https://www.tiktok.com/@hktd_roblox
 
-game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Auto Clicker",
-    Text = "By HKTD",
-	Icon = "rbxassetid://123653870026944",
-    Duration = 5
-})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HKTD-Roblox/Script-Beta/refs/heads/main/Mobile-Keyborad.lua", true))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostPlayer352/Test4/refs/heads/main/Auto%20Clicker"))()
